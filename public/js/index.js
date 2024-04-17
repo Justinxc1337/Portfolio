f9f890c76d343cea55e43ea1a047e9f07b126d3d
Justinxc1337 <110351655+Justinxc1337@users.noreply.github.com>
2024-04-17 15:02:22 +0200

    darkmode test and sticky button start

---
+++ public/js/index.js
@@ -0,0 +1,27 @@
+function darkMode() {
+    let element = document.body;
+    let content = document.getElementById("DarkModetext");
+    element.className = "dark-mode";
+    content.style.color = "#f4d58dff";
+    content.innerText = "Dark Mode is ON";
+}
+
+function lightMode() {
+    let element = document.body;
+    let content = document.getElementById("DarkModetext");
+    element.className = "light-mode";
+    content.style.color = "#f4d58dff";
+    content.innerText = "Dark Mode is OFF";
+}
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    let darkModeToggle = document.getElementById('darkModeToggle');
+    darkModeToggle.checked = true;
+    darkModeToggle.addEventListener('change', function() {
+        if (this.checked) {
+            darkMode();
+        } else {
+            lightMode();
+        }
+    });
+});
