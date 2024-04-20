9f84bd983b1a860b66786cf4a5cf4801e8dff101
Justinxc1337 <110351655+Justinxc1337@users.noreply.github.com>
2024-04-20 19:38:13 +0200

    hopefully last test for mobile

--- public/js/techstacks.js
+++ public/js/techstacks.js
@@ -1,13 +1,9 @@
-// Wait for the DOM content to load
 document.addEventListener("DOMContentLoaded", function() {
-    // Get the menu element
     var x = document.getElementById("myLinks");
     
-    // Set the initial display to none
     x.style.display = "none";
 });
 
-// Toggle the menu's visibility
 function myFunction() {
     var x = document.getElementById("myLinks");
     if (x.style.display === "block") {
