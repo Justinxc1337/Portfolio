c9d3a449c85a705c787c27c3e7269f6100f0a6d6
Justinxc1337 <110351655+Justinxc1337@users.noreply.github.com>
2024-04-20 19:35:14 +0200

    weird fix

--- public/js/techstacks.js
+++ public/js/techstacks.js
@@ -1,8 +1,18 @@
+// Wait for the DOM content to load
+document.addEventListener("DOMContentLoaded", function() {
+    // Get the menu element
+    var x = document.getElementById("myLinks");
+    
+    // Set the initial display to none
+    x.style.display = "none";
+});
+
+// Toggle the menu's visibility
 function myFunction() {
     var x = document.getElementById("myLinks");
     if (x.style.display === "block") {
-    x.style.display = "none";
+        x.style.display = "none";
     } else {
-    x.style.display = "block";
+        x.style.display = "block";
     }
 }
