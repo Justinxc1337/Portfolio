84c4a1028d5c061fa483f7c92b07948a77a7eeb4
Justinxc1337 <110351655+Justinxc1337@users.noreply.github.com>
2024-04-21 01:50:25 +0200

    better game page mobile version support and other screen sizes fix

--- public/js/index.js
+++ public/js/index.js
@@ -25,3 +25,18 @@ document.addEventListener('DOMContentLoaded', (event) => {
         }
     });
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+    var x = document.getElementById("myLinks");
+    
+    x.style.display = "none";
+});
+
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
