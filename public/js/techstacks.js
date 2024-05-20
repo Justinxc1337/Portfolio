898015a69c12d4a700bc051485000009f731fe91
Justinxc1337 <110351655+Justinxc1337@users.noreply.github.com>
2024-05-20 23:45:36 +0200

    animated text

--- public/js/techstacks.js
+++ public/js/techstacks.js
@@ -12,3 +12,14 @@ function myFunction() {
         x.style.display = "block";
     }
 }
+
+const words = ['Coming Soon', 'Frontend', 'Language', 'Framework'];
+let index = 0;
+
+function animateText() {
+    document.getElementById('caption1').textContent = words[index];
+    document.getElementById('caption2').textContent = words[index];
+    index = (index + 1) % words.length;
+}
+
+setInterval(animateText, 2000);
