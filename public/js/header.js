ff8e43183d14d04daf8cd0c8b7fcbc8c7085e00e
LexRoZa <justinxc10@gmail.com>
2025-02-26 01:49:57 +0100

    Contactinfo and new CV

--- public/js/header.js
+++ public/js/header.js
@@ -17,3 +17,18 @@ function closeMenuOnClickOutside(event) {
     document.removeEventListener('click', closeMenuOnClickOutside);
     }
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+    var x = document.getElementById("myLinks");
+    
+    x.style.display = "none";
+});
+
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
