5bfec4f15deab70f661f25f887a3318f752670b3
LexRoZa <justinxc10@gmail.com>
2026-02-21 00:18:57 +0100

    Color change and menu change

--- public/js/index.js
+++ public/js/index.js
@@ -1,44 +1,44 @@
 function darkMode() {
-    let element = document.body;
-    let content = document.getElementById("DarkModetext");
-    element.className = "dark-mode";
-    content.style.color = "#f4d58dff";
-    content.innerText = "Dark Mode is ON";
+  let element = document.body;
+  let content = document.getElementById('DarkModetext');
+  element.className = 'dark-mode';
+  content.style.color = '#ffd166';
+  content.innerText = 'Dark Mode is ON';
 }
 
 function lightMode() {
-    let element = document.body;
-    let content = document.getElementById("DarkModetext");
-    element.className = "light-mode";
-    content.style.color = "#f4d58dff";
-    content.innerText = "Dark Mode is OFF";
+  let element = document.body;
+  let content = document.getElementById('DarkModetext');
+  element.className = 'light-mode';
+  content.style.color = '#ffd166';
+  content.innerText = 'Dark Mode is OFF';
 }
 
 document.addEventListener('DOMContentLoaded', (event) => {
-    let darkModeToggle = document.getElementById('darkModeToggle');
-    darkModeToggle.checked = true;
-    darkModeToggle.addEventListener('change', function() {
-        if (this.checked) {
-            darkMode();
-        } else {
-            lightMode();
-        }
-    });
+  let darkModeToggle = document.getElementById('darkModeToggle');
+  darkModeToggle.checked = true;
+  darkModeToggle.addEventListener('change', function () {
+    if (this.checked) {
+      darkMode();
+    } else {
+      lightMode();
+    }
+  });
 });
 
-document.addEventListener("DOMContentLoaded", function() {
-    var x = document.getElementById("myLinks");
-    
-    x.style.display = "none";
+document.addEventListener('DOMContentLoaded', function () {
+  var x = document.getElementById('myLinks');
+
+  x.style.display = 'none';
 });
 
 function myFunction() {
-    var x = document.getElementById("myLinks");
-    if (x.style.display === "block") {
-        x.style.display = "none";
-    } else {
-        x.style.display = "block";
-    }
+  var x = document.getElementById('myLinks');
+  if (x.style.display === 'block') {
+    x.style.display = 'none';
+  } else {
+    x.style.display = 'block';
+  }
 }
 
 /* document.addEventListener('DOMContentLoaded', (event) => {
@@ -93,40 +93,40 @@ function myFunction() {
 }); */
 
 document.addEventListener('DOMContentLoaded', (event) => {
-    const swiper = new Swiper('.swiper', {
-        effect: 'coverflow',
-        grabCursor: true,
-        centeredSlides: true,
-        slidesPerView: 'auto',
-        coverflowEffect: {
-            rotate: 50,
-            stretch: 0,
-            depth: 100,
-            modifier: 1,
-            slideShadows: true,
-        },
-        loop: true,
-        autoplay: {
-            delay: 3000,
-        },
-        pagination: {
-            el: '.swiper-pagination',
-            clickable: true,
-        },
+  const swiper = new Swiper('.swiper', {
+    effect: 'coverflow',
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    coverflowEffect: {
+      rotate: 50,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: true,
+    },
+    loop: true,
+    autoplay: {
+      delay: 3000,
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+    breakpoints: {
+      480: {
         navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
+          nextEl: null,
+          prevEl: null,
         },
-        scrollbar: {
-            el: '.swiper-scrollbar',
-        },
-        breakpoints: {
-            480: {
-                navigation: {
-                    nextEl: null,
-                    prevEl: null,
-                }
-            }
-        }
-    });
+      },
+    },
+  });
 });
